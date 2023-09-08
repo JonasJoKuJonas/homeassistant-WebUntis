a8598de7421bedf8b7c66e4f298711f3eac66a69
Jonas <K104Jonas@gmail.com>
2023-09-08 19:08:09 +0000

    fixes service

--- custom_components/webuntis/services.py
+++ custom_components/webuntis/services.py
@@ -6,44 +6,50 @@ import logging
 from datetime import datetime
 
 from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.service import async_extract_config_entry_ids
 
 from .const import DOMAIN
+from .utils import get_schoolyear
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_services(hass: HomeAssistant) -> None:
-    """Set up services for Fritz integration."""
+    """Set up services for WebUntis integration."""
 
     if hass.services.has_service(DOMAIN, "get_timetable"):
         return
 
-    async def async_call_fritz_service(service_call: ServiceCall) -> None:
-        """Call correct Fritz service."""
+    async def async_call_webuntis_service(service_call: ServiceCall) -> None:
+        """Call correct WebUntis service."""
 
         entry_id = await async_extract_config_entry_ids(hass, service_call)
         config_entry = hass.config_entries.async_get_entry(list(entry_id)[0])
-
         webuntis_object = hass.data[DOMAIN][config_entry.unique_id]
 
         data = service_call.data
 
         start_date = datetime.strptime(data["start"], "%Y-%m-%d")
-
         end_date = datetime.strptime(data["end"], "%Y-%m-%d")
 
+        if end_date < start_date:
+            raise HomeAssistantError(f"Start date has to be bevor end date")
+
+        if not get_schoolyear(webuntis_object.school_year, date=start_date.date()):
+            raise HomeAssistantError(f"Start date is not in any schoolyear")
+
         timetable = await hass.async_add_executor_job(
-            webuntis_object._get_events_timerange, start_date, end_date
+            webuntis_object._get_events_in_timerange, start_date, end_date
         )
 
-        timetable = {"test": "ds"}
+        timetable_dict = {index: value for index, value in enumerate(timetable)}
 
-        return timetable
+        return timetable_dict
 
     hass.services.async_register(
         DOMAIN,
         "get_timetable",
-        async_call_fritz_service,
+        async_call_webuntis_service,
         supports_response=SupportsResponse.ONLY,
     )
